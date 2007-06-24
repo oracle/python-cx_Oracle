43d4d2b287be7c656f3fb6a45fe0a2d906a9e1e8
Anthony Tuininga <anthony.tuininga@gmail.com>
2007-06-24 04:33:21 +0000

    Building document is now being done on my home machine which has a different location for the Python source.

--- doc/build.sh
+++ doc/build.sh
@@ -1,4 +1,4 @@
-python /u01/subversion/python/trunk/Doc/tools/mkhowto \
+python /u01/svn/python/trunk/Doc/tools/mkhowto \
 	--html --dir ../html cx_Oracle.tex 
 rm -rf ../html/WARNINGS
 rm -f ../html/index.dat
