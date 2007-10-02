ed13b8314def8a3461f976ea6d7a6e77ec75ebd0
Anthony Tuininga <anthony.tuininga@gmail.com>
2007-10-02 02:50:53 +0000

    Tweaked documentation in preparation for release of version 4.3.3.

--- doc/build.sh
+++ doc/build.sh
@@ -1,4 +1,4 @@
-python /u01/svn/python/trunk/Doc/tools/mkhowto \
+python /u01/svn/python/release25-maint/Doc/tools/mkhowto \
 	--html --dir ../html cx_Oracle.tex 
 rm -rf ../html/WARNINGS
 rm -f ../html/index.dat
