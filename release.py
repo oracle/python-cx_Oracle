e438e404b15cf73dca443172184bfb50ee5b917f
Anthony Tuininga <anthony.tuininga@gmail.com>
2009-10-18 03:19:33 +0000

    Tweaked release.py in order to use environment variables to define which Python versions to use for the release and also how to execute it. This is primarily because Mac OS X and 64-bit Windows do not support some of the older Python versions very well.

--- release.py
+++ release.py
@@ -6,22 +6,15 @@ platform.
 import os
 import sys
 
-pythonVersions = [
-        (2, 4),
-        (2, 5),
-        (2, 6),
-        (3, 1)
-]
-
-currentVersion = sys.version_info[:2]
-pythonVersions.remove(currentVersion)
-pythonVersions.append(currentVersion)
-oracleHomes = os.environ["ORACLE_HOMES"].split(",")
+oracleHomes = os.environ["CX_ORACLE_HOMES"].split(",")
+pythonVersions = os.environ["CX_ORACLE_PYTHON_VERSIONS"].split(",")
+pythonFormat = os.environ["CX_ORACLE_PYTHON_FORMAT"]
 
 for withUnicode in (False, True):
     if withUnicode:
         os.environ["WITH_UNICODE"] = "1"
-    for majorVersion, minorVersion in pythonVersions:
+    for version in pythonVersions:
+        majorVersion, minorVersion = [int(s) for s in version.split(".")]
         if withUnicode and majorVersion >= 3:
             continue
         for oracleHome in oracleHomes:
@@ -29,21 +22,17 @@ for withUnicode in (False, True):
                     (majorVersion, minorVersion, oracleHome)
             sys.stdout.write("Creating release %s.\n" % messageFragment)
             os.environ["ORACLE_HOME"] = oracleHome
+            python = pythonFormat % (majorVersion, minorVersion)
             if sys.platform == "win32":
-                python = "c:/python%s%s/python" % (majorVersion, minorVersion)
                 if majorVersion == 2 and minorVersion == 4:
                     subCommand = "bdist_wininst"
                 else:
                     subCommand = "bdist_msi"
                 command = "%s setup.py %s" % (python, subCommand)
-                sys.stdout.write("Running command %s\n" % command)
-                if os.system(command) != 0:
-                    sys.exit("Stopping. Build %s failed.\n" % messageFragment)
             else:
-                python = "python%s.%s" % (majorVersion, minorVersion)
                 command = "%s setup.py bdist_rpm --no-autoreq --python %s" % \
                         (python, python)
-                sys.stdout.write("Running command %s\n" % command)
-                if os.system(command) != 0:
-                    sys.exit("Stopping. Build %s failed.\n" % messageFragment)
+            sys.stdout.write("Running command %s\n" % command)
+            if os.system(command) != 0:
+                sys.exit("Stopping. Build %s failed.\n" % messageFragment)
 
