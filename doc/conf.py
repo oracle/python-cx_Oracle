2646230de3a1abc5db9b4988550eacf9a37d2a1f
Anthony Tuininga <anthony.tuininga@gmail.com>
2010-02-05 05:13:25 +0000

    Bump copyright into 2010.

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'cx_Oracle'
-copyright = '2009, Anthony Tuininga'
+copyright = '2010, Anthony Tuininga'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
