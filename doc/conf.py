bccdfccea83e484246d3ada513ad506c12a5c92e
Anthony Tuininga <anthony.tuininga@gmail.com>
2011-03-20 04:57:10 +0000

    Preparing to release 5.1.

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'cx_Oracle'
-copyright = '2010, Anthony Tuininga'
+copyright = '2011, Anthony Tuininga'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
