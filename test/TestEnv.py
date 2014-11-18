2ea3b9d37a5b9f465400a7fd593ef1f86652a031
tchaumeny <devnull@localhost>
2014-11-18 23:15:36 +0100

    Made test suite Python3 compatible

--- test/TestEnv.py
+++ test/TestEnv.py
@@ -5,10 +5,13 @@ import os
 import sys
 import unittest
 
+if sys.version_info[0] < 3:
+    input = raw_input
+
 def GetValue(name, label):
     value = os.environ.get("CX_ORACLE_" + name)
     if value is None:
-        value = raw_input(label + ": ")
+        value = input(label + ": ")
     if hasattr(cx_Oracle, "UNICODE") or sys.version_info[0] >= 3:
         return value
     return unicode(value)
