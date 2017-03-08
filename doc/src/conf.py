431a6d7ddb0a5cac5c1d63e0b820daf9778f043c
Anthony Tuininga <anthony.tuininga@oracle.com>
2017-03-08 16:06:05 -0700

    Added all copyright notices as requested.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -33,7 +33,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'cx_Oracle'
-copyright = '2016, 2017 Oracle and/or its affiliates.  All rights reserved.'
+copyright = '2016, 2017, Oracle and/or its affiliates. All rights reserved. Portions Copyright © 2007-2015, Anthony Tuininga. All rights reserved. Portions Copyright © 2001-2007, Computronix (Canada) Ltd., Edmonton, Alberta, Canada. All rights reserved'
 author = 'Oracle'
 
 # The default replacements for |version| and |release|, also used in various
