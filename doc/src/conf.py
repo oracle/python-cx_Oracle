98d35e67b3dd346d47cd79e4c6d909df539c71e2
Anthony Tuininga <anthony.tuininga@oracle.com>
2018-03-07 16:18:01 -0700

    Bump version to 6.3-dev in anticipation of new features landing.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -40,9 +40,9 @@ author = 'Oracle'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '6.2'
+version = '6.3'
 # The full version, including alpha/beta/rc tags.
-release = '6.2.1'
+release = '6.3.0-dev'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
